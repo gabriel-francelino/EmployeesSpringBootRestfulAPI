66430fbfab8ec1dbaa80cb5f6a81c744d720cd76
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-02-09 14:39:54 -0300

    refact: corrigindo mensagem de exceção

--- src/main/java/com/gabriel/exception/OrderNotFoundException.java
+++ src/main/java/com/gabriel/exception/OrderNotFoundException.java
@@ -2,6 +2,6 @@ package com.gabriel.exception;
 
 public class OrderNotFoundException extends RuntimeException{
     public OrderNotFoundException(Long id) {
-        super("Not found employee with id " + id);
+        super("Not found order with id " + id);
     }
 }
